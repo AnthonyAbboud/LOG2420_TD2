325e62892a259b919cd230f3d2460b9e4007de64
nana <rochanafardon@hotmail.fr>
2018-10-24 09:55:36 -0400

    connexion ws

---
+++ connectionHandler.js
@@ -0,0 +1,10 @@
+var exampleSocket = new WebSocket("ws://log2420-nginx.info.polymtl.ca/chatservice");
+
+exampleSocket.onopen = function () {
+     console.log("Web Socket open!"); }
+
+     exampleSocket.onerror = function () 
+     { console.log("ERROR"); }
+
+     exampleSocket.onmessage = function () 
+     { console.log("Message Receieved"); } 
