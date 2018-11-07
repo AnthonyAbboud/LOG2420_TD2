857bc2375eacfac8474adefa23b5bc161b198829
Beryl Sabourin <besabb@l4714-10.info.polymtl.ca>
2018-11-07 11:05:52 -0500

    first push

--- connectionHandler.js
+++ connectionHandler.js
@@ -1,15 +1,69 @@
+
+/* Voir ce site : 
+    https://developer.mozilla.org/fr/docs/WebSockets/Writing_WebSocket_client_applications#Utilisation_de_JSON_pour_transmettres_des_objets
+*/
+
+
 var exampleSocket = new WebSocket("ws://log2420-nginx.info.polymtl.ca/chatservice");
 
-    exampleSocket.onopen = function () {
-     console.log("Web Socket open!"); }
+exampleSocket.onopen = function () 
+{
+    console.log("Web Socket opened!");
+}
 
-     exampleSocket.onerror = function () 
-     { console.log("ERROR"); }
+exampleSocket.onerror = function () 
+{ 
+    console.log("ERROR"); 
+}
 
-    //  exampleSocket.onmessage = function () 
-    //  { console.log("Message Receieved"); } 
 
-     
-     
+exampleSocket.onmessage = function (event) 
+{
+    console.log("ok");
+  
+    var text = "";
+    var msg = JSON.parse(event.data);
+    var time = new Date(msg.date);
+    var timeStr = time.toLocaleTimeString();    
+    console.log(msg);
 
+    if(msg.channelId="666"){
+
+      switch(msg.eventType) {
+        case "id":
+          clientID = msg.id;
+          setUsername();
+          break;
+        case "username":
+          text = "<b>User <em>" + msg.name + "</em> signed in at " + timeStr + "</b><br>";
+          break;
+        case "onMessage":
+        {
+          text = "(" + timeStr + ") <b>" + msg.name + "</b>: " + msg.text + "<br>";
+        }
+          
+          break;
+        case "rejectusername":
+          text = "<b>Your username has been set to <em>" + msg.name + "</em> because the name you chose is in use.</b><br>"
+          break;
+        case "userlist":
+          var ul = "";
+          for (i=0; i < msg.users.length; i++) {
+            ul += msg.users[i] + "<br>";
+          }
+          document.getElementById("userlistbox").innerHTML = ul;
+          break;
+      }
+
+
+    }
     
+    
+    
+   
+    }
+  
+  // exampleSocket.close();
+
+
+        
--- message.js
+++ message.js
@@ -1,3 +1,10 @@
+/* Aide : 
+https://www.xul.fr/html5/websocket.php 
+
+https://developer.mozilla.org/fr/docs/WebSockets/Writing_WebSocket_client_applications#Utilisation_de_JSON_pour_transmettres_des_objets
+*/ 
+
+
 /**
  * @description Message, Represents a message DTO (data transfert object).
  * @author Tanga Mathieu Kaboré
@@ -22,3 +29,48 @@ class Message {
         this.timestamp = timestamp;
     }
 }
+
+// Envoi des données vers le serveur en JSON
+function sendText() {
+  console.log("sendtext");
+    // Création d'un objet msg qui contient les données dont le serveur a besoin pour traiter le message
+    var msg = new Message("onMessage", null, document.getElementById("text").value, null, Date.now());
+
+    // Envoi de l'objet msg à travers une chaîne formatée en JSON
+    if (exampleSocket.readyState==1) {
+      exampleSocket.send(JSON.stringify(msg));
+      console.log("fait");
+    }
+        
+    // Efface le texte de l'élément input afin de recevoir la prochaine ligne de texte que l'utilisateur va saisir
+    document.getElementById("text").value = "";
+}
+
+document.getElementById("bouton_envoyer").addEventListener("keypress", function(e){
+  var key = e.which || e.keyCode;
+  if(key == 13){
+    sendText();
+  }
+});
+document.getElementById("bouton_envoyer").addEventListener("click", sendText);
+
+//Réception des données du serveur et interpretation JSON
+//voir connectionHandler.js
+
+
+
+  // Ajouter un EventListener
+
+/*
+
+  var bouton = document.getElementsByClassName("bouton_envoyer"); //bouton d'envoi du message 
+
+  bouton.addEventListener('click',function(e){ // on écoute l'évènement 'click' sur le bouton permettant d'envoyer le message 
+  e.preventDefault(); // on stoppe la propagation 
+  exampleSocket.sendMessage(); // on envoie un message vers le serveur
+  console.log("boucle eventListener");
+  return false;
+} , true) ; 
+
+*/ 
+
